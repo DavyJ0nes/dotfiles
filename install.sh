c007749ca6c8175299c2576522771d3c5a57bcf5
DavyJ0nes <davy.jones@me.com>
2018-07-03 12:09:08 +0100

    Updated Start process with Retrospective

--- install.sh
+++ install.sh
@@ -22,17 +22,22 @@ THISDIR=$(pwd)
 
 ## Symlink vim files
 echo "Linking vim files"
+
 if ! ls "$HOME/.vimrc" > /dev/null 2>&1; then
   ln -s "$THISDIR"/vim/vimrc "$HOME"/.vimrc
 fi
+
 if ! ls "$HOME/.vim" > /dev/null 2>&1; then
   ln -s "$THISDIR"/vim/vim "$HOME"/.vim
 fi
+
 ## Symlink zsh files
 echo "Linking zsh files"
+
 if ! ls "$HOME/.zshrc" > /dev/null 2>&1; then
   ln -s "$THISDIR"/zsh/zshrc "$HOME"/.zshrc
 fi
+
 if ! ls "$HOME/.oh-my-zsh" > /dev/null 2>&1; then
   ln -s "$THISDIR"/zsh/oh-my-zsh "$HOME"/.oh-my-zsh
 fi

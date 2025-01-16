d4e3e4b01cda32f5efad9bcc5452c9e1ad27dcde
Davy Jones <davy.jones@einride.tech>
2025-01-16 20:31:43 +0100

    feat: more tweaks

---
+++ .config/nvim/lua/davyj0nes/plugins/taskwarrior.lua
@@ -0,0 +1,25 @@
+return {
+	"ribelo/taskwarrior.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	-- or
+	config = true,
+	keys = {
+		{
+			"<leader>td",
+			desc = "Taskwarrior",
+		},
+		{
+			"<leader>ftd",
+			function()
+				require("taskwarrior_nvim").browser({ "ready" })
+			end,
+			desc = "Ongoing tasks",
+			mode = "n",
+			noremap = true,
+		},
+	},
+}
